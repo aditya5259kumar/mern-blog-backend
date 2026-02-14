af3cc0a70c0137d4e7e23e242894b89a0dba303f
adtya5259kumar <aditya5259kumar@gmail.com>
2026-02-14 11:24:57 +0530

    modified db.js ,userAuthController.js and authValidator.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/blog_app");
+    await mongoose.connect(process.env.MONGO_DB_URL);
     console.log("mongoDB connected successfully.");
   } catch (error) {
     console.log("failed to connect mongoDB!");
